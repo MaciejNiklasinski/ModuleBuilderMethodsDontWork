d79cf3ade2d07a2999e7fb589f5c45d4f1a85f5f
Maciej Niklasinski <maciej.niklasinski@gmail.com>
2022-09-15 17:50:11 +0100

    Added buttons trigerring native functions

--- App.js
+++ App.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import type {Node} from 'react';
-import {Text} from 'react-native';
+import {Button, View, NativeModules } from 'react-native';
+
+const { FancyMath, FancyMathNoAttr } = NativeModules;
 
 const App: () => Node = () => {
-
-  return <Text>Rendered</Text>
+  return (  
+    <View style={{
+      height: "100%",
+      display: "flex",
+      justifyContent: "space-around"
+    }}>
+      <Button
+        title='FancyMath'
+        onPress={(e) => {
+          try {
+            FancyMath.add(1.1, 1.1, (sum) => {
+              alert(sum); 
+            });
+          } catch (error) {
+            alert(error);
+          }
+        }} />
+      <Button
+        title='FancyMathNoAttr'
+        onPress={(e) => {
+          try {
+            FancyMathNoAttr.add(1.1, 1.1, (sum) => {
+              alert(sum); 
+            });
+          } catch (error) {
+            alert(error);
+          }
+        }} />
+    </View>
+  );
 };
 
 export default App;
